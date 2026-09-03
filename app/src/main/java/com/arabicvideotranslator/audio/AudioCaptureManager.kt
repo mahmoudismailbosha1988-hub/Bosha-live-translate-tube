c4e1221067d4411692060e145f51ff4d1ea1a073
Bosha@venom <mahmoudismailbosha1988@gmail.com>
2026-09-03 22:46:41 +0300

    Create AudioCaptureManager.kt

---
+++ app/src/main/java/com/arabicvideotranslator/audio/AudioCaptureManager.kt
@@ -0,0 +1,12 @@
+package com.arabicvideotranslator.audio
+
+class AudioCaptureManager {
+
+    fun start() {
+        // سيتم إضافة التقاط صوت الفيديو هنا لاحقًا
+    }
+
+    fun stop() {
+        // سيتم إضافة إيقاف التقاط الصوت هنا لاحقًا
+    }
+}
