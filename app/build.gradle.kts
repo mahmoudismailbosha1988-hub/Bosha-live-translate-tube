31e5c79197e7940cce5dd032e40fb1203b9b18cc
Bosha@venom <mahmoudismailbosha1988@gmail.com>
2026-09-04 07:44:22 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,9 +19,14 @@ android {
     buildFeatures {
         compose = true
     }
-    kotlinOptions {
+    compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlinOptions {
     jvmTarget = "17"
-    }
+}
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
