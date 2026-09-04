3798feefa7490e8c9cb2bee9e10e6e3147293ed6
Bosha@venom <mahmoudismailbosha1988@gmail.com>
2026-09-04 06:28:57 +0300

    Create AudioCaptureService.kt

---
+++ app/src/main/java/com/arabicvideotranslator/audio/AudioCaptureService.kt
@@ -0,0 +1,12 @@
+package com.arabicvideotranslator.audio
+
+class AudioCaptureService {
+
+    fun startCapture() {
+        // سيتم تشغيل التقاط صوت الفيديو هنا لاحقًا
+    }
+
+    fun stopCapture() {
+        // سيتم إيقاف التقاط الصوت هنا لاحقًا
+    }
+}
