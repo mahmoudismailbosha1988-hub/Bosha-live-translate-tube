8c4622de4e40aeb2c7092fc2103bbadaec40d767
Bosha@venom <mahmoudismailbosha1988@gmail.com>
2026-09-04 06:21:41 +0300

    Create OverlayService.kt

---
+++ app/src/main/java/com/arabicvideotranslator/OverlayService.kt
@@ -0,0 +1,12 @@
+package com.arabicvideotranslator
+
+class OverlayService {
+
+    fun showSubtitle(text: String) {
+        // سيتم عرض الترجمة العربية فوق الفيديو هنا لاحقًا
+    }
+
+    fun hideSubtitle() {
+        // سيتم إخفاء الترجمة هنا لاحقًا
+    }
+}
