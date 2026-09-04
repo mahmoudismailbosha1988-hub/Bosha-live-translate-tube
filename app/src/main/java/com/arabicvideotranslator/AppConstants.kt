1f06be5bc2b094f648d0e6a53e217fdcf0d3ad6c
Bosha@venom <mahmoudismailbosha1988@gmail.com>
2026-09-04 06:50:27 +0300

    Create AppConstants.kt

---
+++ app/src/main/java/com/arabicvideotranslator/AppConstants.kt
@@ -0,0 +1,9 @@
+package com.arabicvideotranslator
+
+object AppConstants {
+
+    const val SOURCE_LANGUAGE = "en"
+    const val TARGET_LANGUAGE = "ar"
+
+    const val APP_NAME = "Bosha Live Translate Tube"
+}
