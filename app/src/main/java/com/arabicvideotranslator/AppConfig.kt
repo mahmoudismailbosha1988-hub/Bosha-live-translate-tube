9aa68ff61679192e83670ef22f8faf3e165e6091
Bosha@venom <mahmoudismailbosha1988@gmail.com>
2026-09-04 06:51:24 +0300

    Create AppConfig.kt

---
+++ app/src/main/java/com/arabicvideotranslator/AppConfig.kt
@@ -0,0 +1,9 @@
+package com.arabicvideotranslator
+
+object AppConfig {
+
+    const val DEFAULT_SOURCE_LANGUAGE = "en"
+    const val DEFAULT_TARGET_LANGUAGE = "ar"
+
+    const val SUBTITLE_UPDATE_INTERVAL_MS = 500L
+}
