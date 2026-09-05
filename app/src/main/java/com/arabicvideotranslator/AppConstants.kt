ab5ef3a70e17ea61ae9561ec24c637c94401385c
Bosha@venom <mahmoudismailbosha1988@gmail.com>
2026-09-05 11:03:44 +0300

    Add audio constants for translation settings

--- app/src/main/java/com/arabicvideotranslator/AppConstants.kt
+++ app/src/main/java/com/arabicvideotranslator/AppConstants.kt
@@ -2,8 +2,16 @@ package com.arabicvideotranslator
 
 object AppConstants {
 
+    const val SAMPLE_RATE = 16000
+
+    const val CHANNELS = 1
+
+    const val AUDIO_BUFFER_SIZE = 1600
+
     const val SOURCE_LANGUAGE = "en"
+
     const val TARGET_LANGUAGE = "ar"
 
-    const val APP_NAME = "Bosha Live Translate Tube"
+    const val OVERLAY_TEXT = "الترجمة ستظهر هنا"
+
 }
